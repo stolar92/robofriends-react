7015d453a906281a0a673fa5b80362dcfb9e1f71
Patryk Stolar <patrykstolar@gmail.com>
2019-03-12 14:35:47 +0100

    updated App.js with new component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll'
 import './App.css'
 
 
@@ -37,7 +38,9 @@ class App extends Component {  // stworzono component app oparty na klasie (chyb
                 <div className="tc">
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
                     <CardList robots={filteredRobots}/>
+                    </Scroll>
                 </div>
             );
         }
